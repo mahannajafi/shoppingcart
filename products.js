4177e7b0e17857b5a01c321b818736c7f9005201
mahannajafi <mahan.nwork@gmail.com>
2021-10-10 14:49:29 +0330

    finished 0.01

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "queen panel bed",
+    price: 10.99,
+    imageUrl: "./images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: " panel bed",
+    price: 14.99,
+    imageUrl: "./images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: " panel bed",
+    price: 7.99,
+    imageUrl: "./images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "twin panel bed",
+    price: 11.99,
+    imageUrl: "./images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "bed",
+    price: 4.99,
+    imageUrl: "./images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "beed",
+    price: 23.99,
+    imageUrl: "./images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "beeed",
+    price: 42.99,
+    imageUrl: "./images/product-7.jpeg",
+  },
+  {
+    id: 8,
+    title: "beeeeed",
+    price: 24.99,
+    imageUrl: "./images/product-8.jpeg",
+  },
+];
